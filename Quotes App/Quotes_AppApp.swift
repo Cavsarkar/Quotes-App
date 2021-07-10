d304f63d90fd4068f8ac0017ea5b6618742f6f23
Chinmay Avsarkar <chinmay.avsarkar@gmail.com>
2021-07-10 13:25:26 -0500

    Initial Commit

---
+++ Quotes App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes App
+//
+//  Created by Chinmay Avsarkar on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
