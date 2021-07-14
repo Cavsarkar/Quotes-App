34acf527cc3fdd7eb58c58ef099439386e4ea621
Joseph Shepin <jshepin@gmail.com>
2021-07-14 17:43:13 -0500

    added .gitignore, added missing file

---
+++ Quotes App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes App
+//
+//  Created by Joseph Shepin on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
